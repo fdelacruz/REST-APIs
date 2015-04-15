f6d140d92dd67fa60814825044e2651ae67677c7
Francisco De La Cruz <helloquico@gmail.com>
2015-04-15 00:21:28 -0400

    Implement Middleware

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -11,44 +11,48 @@ var routes = function(Book) {
       res.status(201).send(book);
 
     })
-  .get(function(req, res) {
+    .get(function(req, res) {
 
-    var query = {};
+      var query = {};
 
-    if(req.query.genre){
-      query.genre = req.query.genre;
-    }
+      if(req.query.genre){
+        query.genre = req.query.genre;
+      }
 
-    Book.find(query, function(err, books) {
+      Book.find(query, function(err, books) {
+        if(err)
+          res.status(500).send(err);
+        else
+          res.json(books);
+      });
+    });
+
+  // Middleware
+  bookRouter.use('/:bookId', function(req, res, next) {
+    Book.findById(req.params.bookId, function(err, book) {
       if(err)
         res.status(500).send(err);
-      else
-        res.json(books);
+      else if(book) {
+        req.book = book;
+        next();
+      }
+      else {
+        res.status(404).send('no book found');
+      }
     });
   });
 
   bookRouter.route('/:bookId')
     .get(function(req, res) {
-
-      Book.findById(req.params.bookId, function(err, book) {
-        if(err)
-          res.status(500).send(err);
-        else
-          res.json(book);
-      });
+      res.json(req.book);
     })
     .put(function(req, res) {
-      Book.findById(req.params.bookId, function(err, book) {
-        if(err)
-          res.status(500).send(err);
-        else
-          book.title = req.body.title;
-          book.author = req.body.author;
-          book.genre = req.body.genre;
-          book.read = req.body.read;
-          book.save();
-          res.json(book);
-      });
+      req.book.title = req.body.title;
+      req.book.author = req.body.author;
+      req.book.genre = req.body.genre;
+      req.book.read = req.body.read;
+      req.book.save();
+      res.json(req.book);
     });
 
   return bookRouter;
