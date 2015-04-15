8ddf304d2fe719dab69a7afd199733d73a5e98e8
Francisco De La Cruz <helloquico@gmail.com>
2015-04-15 19:06:25 -0400

    Add Hypermedia to our API

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,39 +1,47 @@
 var bookController = function(Book) {
 
-  var post = function(req, res) {
-    var book = new Book(req.body);
+	var post = function(req, res) {
+		var book = new Book(req.body);
 
-    if(!req.body.title) {
-      res.status(400);
-      res.send('Title is required');
-    }
-    else {
-      book.save();
-      res.status(201);
-      res.send(book);
-    }
-  };
+		if(!req.body.title) {
+			res.status(400);
+			res.send('Title is required');
+		}
+		else {
+			book.save();
+			res.status(201);
+			res.send(book);
+		}
+	};
 
-  var get = function(req, res) {
+	var get = function(req, res) {
 
-      var query = {};
+		var query = {};
 
-      if(req.query.genre){
-        query.genre = req.query.genre;
-      }
+		if(req.query.genre){
+			query.genre = req.query.genre;
+		}
 
-      Book.find(query, function(err, books) {
-        if(err)
-          res.status(500).send(err);
-        else
-          res.json(books);
-      });
-  };
-  
-  return  {
-    post: post,
-    get: get
-  };
+		Book.find(query, function(err, books) {
+			if(err)
+				res.status(500).send(err);
+			else {
+				var returnBooks = [];
+				books.forEach(function(element, index, array) {
+					var newBook = element.toJSON();
+					newBook.links = {};
+					newBook.links.self = 'http://' + req.headers.host + '/api/books/' + newBook._id;
+					returnBooks.push(newBook);
+				});
+				res.json(returnBooks);
+			}
+		});
+	};
+
+	return  {
+		post: post,
+		get: get
+	};
 };
 
 module.exports = bookController;
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -25,46 +25,53 @@ var routes = function(Book) {
 
   bookRouter.route('/:bookId')
     .get(function(req, res) {
-      res.json(req.book);
-    })
-    .put(function(req, res) {
-      req.book.title = req.body.title;
-      req.book.author = req.body.author;
-      req.book.genre = req.body.genre;
-      req.book.read = req.body.read;
-      req.book.save(function(err) {
-        if(err)
-          res.status(500).send(err);
-        else {
-          res.json(req.book);
-        }
-      });
-    })
-    .patch(function(req, res) {
-      if(req.body._id)
-        delete req.body._id;
 
-      for(var p in req.body) {
-        req.book[p] = req.body[p];
+      var returnBook = req.book.toJSON();
+
+      returnBook.links = {};
+      returnBook.links.FilterByThisGenre = 'http://' + req.headers.host +
+        '/api/books/?genre=' + returnBook.genre;
+
+      res.json(returnBook);
+    })
+  .put(function(req, res) {
+    req.book.title = req.body.title;
+    req.book.author = req.body.author;
+    req.book.genre = req.body.genre;
+    req.book.read = req.body.read;
+    req.book.save(function(err) {
+      if(err)
+        res.status(500).send(err);
+      else {
+        res.json(req.book);
       }
-
-      req.book.save(function(err) {
-        if(err)
-          res.status(500).send(err);
-        else {
-          res.json(req.book);
-        }
-      });
-    })
-    .delete(function(req, res){
-      req.book.remove(function(err) {
-        if(err)
-          res.status(500).send(err);
-        else {
-          res.status(204).send('book removed');
-        }
-      });
     });
+  })
+  .patch(function(req, res) {
+    if(req.body._id)
+      delete req.body._id;
+
+    for(var p in req.body) {
+      req.book[p] = req.body[p];
+    }
+
+    req.book.save(function(err) {
+      if(err)
+        res.status(500).send(err);
+      else {
+        res.json(req.book);
+      }
+    });
+  })
+  .delete(function(req, res){
+    req.book.remove(function(err) {
+      if(err)
+        res.status(500).send(err);
+      else {
+        res.status(204).send('book removed');
+      }
+    });
+  });
 
   return bookRouter;
 };
